cea5cdb43d1f3b93f0679eaa7ced5add58470b12
Josh Casara <jcasara@gmail.com>
2024-09-07 14:47:44 -0400

    feat: minimal_example_fastapi.py works at 1080p and 30fps

--- diagnostics/minimal_example_fastapi.py
+++ diagnostics/minimal_example_fastapi.py
@@ -1,45 +1,51 @@
-from fastapi import FastAPI, Response
-from fastapi.responses import StreamingResponse
-import cv2
 import time
+
+import cv2
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 # Initialize the video capture object (0 for the default webcam)
 cap = cv2.VideoCapture(0)
 
-"""
-Testing resolution 640x480
-Supported Frame Rates at 640x480: [5, 7, 10, 15, 20, 24, 30]
-Testing resolution 1280x720
-Supported Frame Rates at 1280x720: [5, 7, 10]
-Testing resolution 1920x1080
-Supported Frame Rates at 1920x1080: [5]
-"""
-
 # Set the camera frame rate and dimensions
-frame_width = 640
-frame_height = 480
+frame_width = 1920
+frame_height = 1080
 frame_rate = 30  # Frames per second
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 cap.set(cv2.CAP_PROP_FPS, frame_rate)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
-print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print(cap.get(cv2.CAP_PROP_FPS))
+# print("Width:", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# print("Height:", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# print("FPS (Set):", cap.get(cv2.CAP_PROP_FPS))
+# print("FOURCC:", cap.get(cv2.CAP_PROP_FOURCC))
 
 def gen_frames():
+    fps_frame_count = 0
+    last_time = time.time()
+    fps = 0
     while True:
-        start_time = time.time()
         success, frame = cap.read()
         if not success:
             break
-        # Calculate FPS
-        fps = 1.0 / (time.time() - start_time)
+
+        # Update FPS calculation
+        fps_frame_count += 1
+        current_time = time.time()
+        elapsed_time = current_time - last_time
+        
+        if elapsed_time > 1.0:
+            fps = fps_frame_count / elapsed_time
+            last_time = current_time
+            fps_frame_count = 0
+        
         # Put FPS on the frame
         cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
         # Encode frame as JPEG
         _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
@@ -53,4 +59,3 @@ def video_feed():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
